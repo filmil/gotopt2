a757d42e5a5f287065099aca54be8423fe2cf39d
Filip Filmar <filmil@gmail.com>
2019-03-31 00:03:13 -0700

    Prints error message and exits on long runtime
    The program should typically take a fraction of a second to execute. If
    you forget to pass in stdin it will block waiting for input.  This
    is usually not what we want, so program will exit early and print an
    error message.
    
    Fixes #5

--- cmd/gotopt2/main.go
+++ cmd/gotopt2/main.go
@@ -2,12 +2,22 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"time"
 
 	"github.com/filmil/gotopt2/pkg/opts"
 )
 
 func main() {
+	go func() {
+		// This goroutine will not run if the program exits before the
+		// pause expires.
+		const pause = 10
+		<-time.After(pause * time.Second)
+		fmt.Fprintf(os.Stderr, "gotopt2: %v seconds passed, did you forget to pass config as stdin?\n", pause)
+		os.Exit(145)
+	}()
 	if err := opts.Run(os.Stdin, os.Args[1:], os.Stdout); err != nil {
 		os.Exit(142)
 	}
